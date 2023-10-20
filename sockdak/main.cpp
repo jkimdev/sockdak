9a45c9e25354a666ede53c42f4933f8ea017aa1d
jkimdev <jkim.dev@icloud.com>
2023-10-20 11:08:27 +0900

    socket initialized successfully

--- sockdak/main.cpp
+++ sockdak/main.cpp
@@ -7,9 +7,24 @@
 
 #include <iostream>
 #include "Common.h"
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
+int socket(
+           int domain,
+           int type,
+           int protocol
+           );
+
+int close(int fd);
 
 int main(int argc, const char * argv[]) {
     // insert code here...
-    std::cout << "Hello, World!\n";
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock == -1) err_quit("socket()");
+    printf("[notice] socket initialized successfully!");
+    
+    close(sock);
     return 0;
 }
